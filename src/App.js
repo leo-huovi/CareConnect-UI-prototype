87a1180a76c01b54799d86f538e93ab4423724fb
mametchi <tinyaqui@gmail.com>
2023-09-11 15:21:56 +0300

    Single-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { StyleSheet, css } from 'aphrodite';
 
 function App() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+    setTimeout(() => setActiveIndex(null), 550);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={css(styles.container)}>
+        <h1>CareConnect - Palvelu</h1>
+
+        <div className={css(styles.gridContainer)}>
+          <div
+              className={`button ${activeIndex === 0 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(0)}
+          >
+            <img src={require('./images/town.png')} alt="Talon tapahtumat" className={css(styles.image)} />
+            <p>Talon tapahtumat</p>
+          </div>
+
+          <div
+              className={`button ${activeIndex === 1 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(1)}
+          >
+            <img src={require('./images/keys.png')} alt="Oma asuntosi" className={css(styles.image)} />
+            <p>Oma asuntosi</p>
+          </div>
+
+          <div
+              className={`button ${activeIndex === 2 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(2)}
+          >
+            <img src={require('./images/fixing.png')} alt="Onko jokin rikki?" className={css(styles.image)} />
+            <p>Onko jokin rikki?</p>
+          </div>
+
+
+          <div
+              className={`button ${activeIndex === 3 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(3)}
+          >
+            <img src={require('./images/medicine.png')} alt="Terveys" className={css(styles.image)} />
+            <p>Terveys</p>
+          </div>
+
+
+        </div>
+
+        <div className={css(styles.divider)} />
+
+
+        <div className={css(styles.gridContainer)}>
+
+
+
+          <div
+              className={`button ${activeIndex === 4 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(4)}
+          >
+            <img src={require('./images/company.png')} alt="Lähiseudun ryhmät"
+                 className={css(styles.image)} />
+            <p>Lähiseudun ryhmät</p>
+          </div>
+
+          <div
+              className={`button ${activeIndex === 5 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(5)}
+          >
+            <img src={require('./images/sports.png')} alt="Liikuntapalvelut" className={css(styles.image)} />
+            <p>Liikuntapalvelut</p>
+          </div>
+
+          <div
+              className={`button ${activeIndex === 6 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(6)}
+          >
+            <img src={require('./images/school.png')} alt="Lähiseudun kurssit" className={css(styles.image)}
+            />
+            <p>Lähiseudun kurssit</p>
+          </div>
+
+          <div
+              className={`button ${activeIndex === 7 ? css(styles.animate) : ''}`}
+              onClick={() => handleClick(7)}
+          >
+            <img src={require('./images/chess.png')} alt="Harrastukset" className={css(styles.image)} />
+            <p>Harrastukset</p>
+          </div>
+        </div>
+      </div>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    textAlign: 'center',
+  },
+  divider: {
+    height: '1px',
+    width: '600px',
+    minHeight: '1px',
+    background: 'gray',
+    margin: '20px 0',
+    '@media (max-width: 900px)': {
+      width: '400px',
+    },
+    '@media (max-width: 600px)': {
+      width: '250px',
+    },
+  },
+  animate: {
+    animationName: {
+      '0%': {
+        transform: 'scale(0.85)'
+      },
+      '50%': {
+        transform: 'scale(1.15)'
+      },
+      '100%': {
+        transform: 'scale(0.85)'
+      }
+    },
+    animationDuration: '550ms',
+    animationTimingFunction: 'ease-in-out'
+  },
+  gridContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gap: '20px',
+    marginBottom: '20px',
+    '@media (max-width: 900px)': {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      width: '500px',
+    },
+    '@media (max-width: 600px)': {
+      gridTemplateColumns: '1fr',
+      width: '300px',
+    },
+  },
+  image: {
+    width: '100px',
+    height: '100px',
+  },
+});
 
 export default App;
